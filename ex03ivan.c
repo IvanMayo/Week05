0d19e6fe21b80d7c9a98cb820a2c41751e0ec199
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-07 09:12:51 -0500

    Create ex03ivan.c

---
+++ ex03ivan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+  float X,   Y;
+  printf("inserte valor de x\n");
+  scanf("%f", &X);
+
+  Y= (X*4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)+(4/17)-(4/19);
+
+printf("el numero es %.3f", Y);
+
+  return 0;
+}
