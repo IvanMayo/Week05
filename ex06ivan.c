ddf04d4b83ed8cb930114119802a36d0eb9a5f5f
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-08 11:52:20 -0500

    Update ex06ivan.c

--- ex06ivan.c
+++ ex06ivan.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
+//you input a year and will tell you if the year is biciesto or not
+//n the Gregorian calendar, three criteria must be taken into account to identify leap years: 
+//The year must be evenly divisible by 4;
+//If the year can also be evenly divided by 100, it is not a leap year;
+//Unless... the year is also evenly divisible by 400. Then it is a leap year.
+
+//According to these rules, the years 2000 and 2400 are leap years,
+//while 1800, 1900, 2100, 2200, 2300, and 2500 are not leap years.
+
+
  int X;
 
 int main(void){
